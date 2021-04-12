f6814ca21bb3836ef83ee3813e5670967ff33654
Daniel Wolfgang Malamud-Roam <dmalamudroam@gmail.com>
2021-04-12 11:36:02 -0700

    Initial bubbleSort implementation

---
+++ Algorithms/bubbleSort.js
@@ -0,0 +1,17 @@
+const bubbleSort = (arr) => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+let testArr = [2,1,4,5,9,-1,0];
+console.log(testArr);
+bubbleSort(testArr);
+console.log(testArr);
