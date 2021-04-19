eb6d9906ca19087f44eb907ec90fd461b19350e0
Daniel Wolfgang Malamud-Roam <dmalamudroam@gmail.com>
2021-04-19 09:45:28 -0700

    Initial Solution

--- Algorithms/quicksort.js
+++ Algorithms/quicksort.js
@@ -1,8 +1,8 @@
 const pivot = (arr, start = 0, end = arr.length - 1) => {
   let pivotIdx = start;
-  let pivotVal = arr[0];
+  let pivotVal = arr[start];
 
-  for (let i = start + 1; i < arr.length; i++) {
+  for (let i = start + 1; i <= end; i++) {
     let current = arr[i];
     if (current < pivotVal) {
       pivotIdx++;
@@ -12,11 +12,24 @@ const pivot = (arr, start = 0, end = arr.length - 1) => {
     }
   }
   let swap = arr[pivotIdx];
-  arr[pivotIdx] = arr[0];
-  arr[0] = swap;
+  arr[pivotIdx] = arr[start];
+  arr[start] = swap;
   return pivotIdx;
 }
 
+const quickSort = (arr, start = 0, end = arr.length - 1) => {
+  if (start >= end) {
+    return arr;
+  }
+
+  let pivotIdx = pivot(arr, start, end);
+  quickSort(arr, start, pivotIdx - 1);
+  quickSort(arr, pivotIdx + 1, end);
+
+  return arr;
+}
+
 let testArr = [-2,-5,6,-9,11,4,4];
-console.log(pivot(testArr));
+console.log(quickSort(testArr));
+//console.log(pivot(testArr));
 console.log(testArr);
